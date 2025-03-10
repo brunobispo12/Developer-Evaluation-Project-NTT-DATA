12a7e855b74c8dd3680a0c23156d35a288b08d27
Bruno Bispo <124530740+brunobispo12@users.noreply.github.com>
2025-03-10 20:00:53 -0300

    chore: remove unused imports

--- tests/Ambev.DeveloperEvaluation.Unit/Application/TestData/CreateSaleHandlerTestData.cs
+++ tests/Ambev.DeveloperEvaluation.Unit/Application/TestData/CreateSaleHandlerTestData.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using Bogus;
+﻿using Bogus;
 using Ambev.DeveloperEvaluation.Application.Sales.CreateSale;
 using Ambev.DeveloperEvaluation.Application.Sales;
 
