980ed7dacaf06819a11bcf0e0af0387e79e0a2c6
Bruno Bispo <124530740+brunobispo12@users.noreply.github.com>
2025-03-10 00:31:51 -0300

    feat: add ef configurations for Sale and SaleItem

---
+++ src/Ambev.DeveloperEvaluation.ORM/Mapping/SaleItemConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+public class SaleItemConfiguration : IEntityTypeConfiguration<SaleItem>
+{
+    public void Configure(EntityTypeBuilder<SaleItem> builder)
+    {
+        builder.HasKey(si => si.Id);
+
+        builder.Property(si => si.Product)
+            .IsRequired()
+            .HasMaxLength(50);
+
+        builder.Property(si => si.Quantity)
+            .IsRequired();
+
+        builder.Property(si => si.UnitPrice)
+            .HasColumnType("decimal(18,2)")
+            .IsRequired();
+
+        builder.Property(si => si.Discount)
+            .HasColumnType("decimal(5,2)")
+            .IsRequired();
+
+        builder.Ignore(si => si.TotalPrice);
+
+        builder.HasOne<Sale>()
+            .WithMany(s => s.Items)
+            .HasForeignKey("SaleId")
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
