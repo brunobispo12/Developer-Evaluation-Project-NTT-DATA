cc8f37c775a7dbca577a6234492eba5498f96b0f
Bruno Bispo <124530740+brunobispo12@users.noreply.github.com>
2025-03-13 15:40:33 -0300

    fix(automapper): correct mapping in UpdateSaleProfile

--- src/Ambev.DeveloperEvaluation.Application/Sales/UpdateSale/UpdateSaleProfile.cs
+++ src/Ambev.DeveloperEvaluation.Application/Sales/UpdateSale/UpdateSaleProfile.cs
@@ -1,4 +1,5 @@
-﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+﻿using Ambev.DeveloperEvaluation.Common.DTO;
+using Ambev.DeveloperEvaluation.Domain.Entities;
 using AutoMapper;
 
 namespace Ambev.DeveloperEvaluation.Application.Sales.UpdateSale
@@ -15,6 +16,31 @@ namespace Ambev.DeveloperEvaluation.Application.Sales.UpdateSale
         {
             CreateMap<UpdateSaleCommand, Sale>();
             CreateMap<Sale, UpdateSaleResult>();
+
+
+            CreateMap<UpdateSaleCommand, Sale>()
+                .ForMember(dest => dest.Items, opt => opt.Ignore())
+                .AfterMap((src, dest) =>
+                {
+                    var saleItems = src.Items.Select(dto =>
+                        new SaleItem(
+                            dest,
+                            dto.Product,
+                            dto.Quantity,
+                            dto.UnitPrice,
+                            dto.Discount
+                        )
+                    ).ToList();
+                    dest.AddItems(saleItems);
+                    if (src.IsCancelled)
+                    {
+                        dest.CancelSale();
+                    }
+                });
+            CreateMap<Sale, UpdateSaleResult>()
+                .ForMember(dest => dest.Sale, opt => opt.MapFrom(src => src));
+            CreateMap<Sale, SaleDto>();
+            CreateMap<SaleItem, SaleItemDto>();
         }
     }
 }
