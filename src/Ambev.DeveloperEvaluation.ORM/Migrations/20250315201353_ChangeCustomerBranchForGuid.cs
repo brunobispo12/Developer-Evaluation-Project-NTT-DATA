e57983a0b5b9f01b0dfd4e494b678c1342ab8c66
Bruno Bispo <124530740+brunobispo12@users.noreply.github.com>
2025-03-15 19:15:52 -0300

    chore(migrations): add migration to convert Customer and Branch columns to Guid

---
+++ src/Ambev.DeveloperEvaluation.ORM/Migrations/20250315201353_ChangeCustomerBranchForGuid.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ambev.DeveloperEvaluation.ORM.Migrations
+{
+    public partial class ChangeCustomerBranchForGuid : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                "UPDATE \"Sales\" " +
+                "SET \"Customer\" = '00000000-0000-0000-0000-000000000000' " +
+                "WHERE NOT (\"Customer\" ~* '^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$');");
+
+            migrationBuilder.Sql(
+                "UPDATE \"Sales\" " +
+                "SET \"Branch\" = '00000000-0000-0000-0000-000000000000' " +
+                "WHERE NOT (\"Branch\" ~* '^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$');");
+
+            migrationBuilder.Sql(
+                "ALTER TABLE \"Sales\" ALTER COLUMN \"Customer\" TYPE uuid USING \"Customer\"::uuid;");
+
+            migrationBuilder.Sql(
+                "ALTER TABLE \"Sales\" ALTER COLUMN \"Branch\" TYPE uuid USING \"Branch\"::uuid;");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                "ALTER TABLE \"Sales\" ALTER COLUMN \"Customer\" TYPE character varying(100) USING \"Customer\"::text;");
+            migrationBuilder.Sql(
+                "ALTER TABLE \"Sales\" ALTER COLUMN \"Branch\" TYPE character varying(100) USING \"Branch\"::text;");
+        }
+    }
+}
